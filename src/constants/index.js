ff3df2c9b8da66662695e60226f477389de76ce8
BrianOdhiambo <brianodhiambo530@gmail.com>
2025-04-01 11:19:36 +0300

    Fixed router bug

--- src/constants/index.js
+++ src/constants/index.js
@@ -103,3 +103,19 @@ export const teamMembers = [
     email: "winny@datawise.africa"
   },
 ];
+
+
+// const baseRoutes = [
+//   { path: "/" },
+//   { path: "/projects" },
+//   { path: "/datalab" },
+//   { path: "/about-us" },
+//   { path: "/contact-us" },
+//   { path: "/jobs" },
+//   { path: "/become-a-partner" },
+//   { path: "/partners" }
+// ];
+
+// export default baseRoutes;
+
+// "generate:sitemap": "node generateSitemap.js"
