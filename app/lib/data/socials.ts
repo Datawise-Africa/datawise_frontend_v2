15abaae810550c9aaa0d41a4be919395df8ad2e1
forinda <forinda82@gmail.com>
2026-03-18 16:14:12 +0300

    feat: Remove datawise github social

--- app/lib/data/socials.ts
+++ app/lib/data/socials.ts
@@ -4,10 +4,6 @@ export const socials = [
     url: 'https://www.linkedin.com/company/datawise-africa',
   },
   {
-    name: 'Github',
-    url: 'https://github.com/Datawise-Africa',
-  },
-  {
     name: 'X',
     url: 'https://x.com/datawise_AFR',
   },
